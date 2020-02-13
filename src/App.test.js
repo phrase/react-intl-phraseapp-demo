709ad92831001e16503d84fa8508cc0966de2a4d
Jakub Freisler <jakubfreisler@phrase.com>
2020-02-13 14:03:33 +0100

    chore: update react-intl-phraseapp version
    fix test suite

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { IntlProvider } from 'react-intl';
 import App from './App';
+import { initializePhraseAppEditor } from 'react-intl-phraseapp';
 
+var config = {
+  projectId: '00453ba3c0763049c73f0a04ed067e66',
+  phraseEnabled: true,
+  prefix: '[[__',
+  suffix: '__]]'
+}
+
+initializePhraseAppEditor(config);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
